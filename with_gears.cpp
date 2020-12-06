bedd0fd57dfe76c5d5d496062ebb2be28b0e4cf2
Yves <37640312+dp-wu@users.noreply.github.com>
2020-12-05 22:39:54 -0800

    remove constructors with no parameters

--- with_gears.cpp
+++ with_gears.cpp
@@ -25,30 +25,29 @@ public:
     int ind;  // the value of total_warrior in HQ
     int counter;
     const string *weapons; // weapon array
-    
-    Warrior() {}
+
     Warrior(string tm, string n, int s, int h, int tw, int c):
         team(tm), name(n), strength(s), HQ_strength(h), ind(tw), counter(c) { weapons = WEAPON_ORDER; }
-    
+
     virtual void print_feature() {}
     void print_common() {
         cout << " " << team << " " << name << " " << ind;
         cout << " born with strength " << strength << "," << counter;
         cout << " " << name << " in " << team << " headquarter" << endl;
     }
+    virtual ~Warrior() {}
 };
 
 class Dragon: public Warrior {
 public:
     string weapon;
     double morale;
-    
-    Dragon() {}
+
     Dragon(string tm, string n, int s, int h, int tw, int c): Warrior(tm, n, s, h, tw, c) {
         weapon = weapons[ind%WEAPON_TYPES];
         morale = (double) HQ_strength / strength;
     }
-    
+
     void set_value(string tm, string n, int s, int h, int tw){
         Warrior::team = tm;
         weapon = weapons[ind%WEAPON_TYPES];
@@ -63,13 +62,12 @@ public:
 class Ninja: public Warrior {
 public:
     string weapon[2];
-    
-    Ninja() {}
+
     Ninja(string tm, string n, int s, int h, int tw, int c): Warrior(tm, n, s, h, tw, c) {
         weapon[0] = weapons[ind%WEAPON_TYPES];
         weapon[1] = weapons[(ind+1)%WEAPON_TYPES];
     }
-    
+
     virtual void print_feature() {
         print_common();
         cout << "It has a " << weapon[0] << " and a " << weapon[1] << endl;
@@ -78,9 +76,8 @@ public:
 
 class Wolf: public Warrior {
 public:
-    Wolf() {}
     Wolf(string tm, string n, int s, int h, int tw, int c): Warrior(tm, n, s, h, tw, c) {}
-    
+
     virtual void print_feature() {
         print_common();
     }
@@ -89,10 +86,9 @@ public:
 class Iceman: public Warrior {
 public:
     string weapon;
-    
-    Iceman() {}
+
     Iceman(string tm, string n, int s, int h, int tw, int c): Warrior(tm, n, s, h, tw, c) { weapon = weapons[ind%WEAPON_TYPES]; }
-    
+
     virtual void print_feature() {
         print_common();
         cout << "It has a " << weapon << endl;
@@ -102,10 +98,10 @@ public:
 class Lion: public Warrior {
 public:
     int loyalty;
-    
-    Lion() {}
+
+//    Lion() {}
     Lion(string tm, string n, int s, int h, int tw, int c): Warrior(tm, n, s, h, tw, c) { loyalty = HQ_strength; }
-    
+
     virtual void print_feature() {
         print_common();
         cout << "It's loyalty is " << loyalty << endl;
@@ -126,20 +122,20 @@ public:
     string team;  // red or blue
     int total_warrior;  // total number of warriors in this HQ
     int total_strength;  // total strength left in the HQ
-    
+
     const string *warrior_order;  // order to produce the warrior (name)
     int prod_order[WARRIOR_TYPES];  // order to produce the warrior (strength)
     int min_strength; // minimum strength needed for producing a warrior
     int p;  // index indicating which warrior we are producing
-    
+
     Warrior *warrior;
     int counter[WARRIOR_TYPES];
     bool end_of_prod; // true if HQ strength is insufficient to produce any warrior
-    
-    HQ(int M, string t, int *arr): team(t), total_strength(M), total_warrior(0) {
+
+    HQ(int M, string t, int *arr): team(t), total_warrior(0), total_strength(M) {
         if (t == "red") warrior_order = RED_ORDER;
         else warrior_order = BLUE_ORDER;
-        
+
         min_strength = MAX_STRENGTH;
         for (int i=0; i<WARRIOR_TYPES; ++i) {
             counter[i] = 0;
@@ -148,9 +144,10 @@ public:
         }
         p = 0;
         end_of_prod = false;
+        warrior = nullptr;
     }
-    //~HQ() { if (warrior) delete warrior; }
-    
+    ~HQ() { if (warrior) delete warrior; }
+
     void create_warrior(int current_time) {
         if (total_strength < min_strength) { end_of_prod = true; }
         else {
@@ -204,7 +201,7 @@ int main() {
     do {
         // read in initial strength of HQs
         cin >> M; cin.ignore(1000, 10);
-        
+
         // read in each warrior's strength needed for production
         Stats slist[WARRIOR_TYPES];
         slist[0].name = 'd';
@@ -213,7 +210,7 @@ int main() {
         slist[3].name = 'l';
         slist[4].name = 'w';
         for (int i=0; i<WARRIOR_TYPES; ++i) cin >> slist[i].strength;
-        
+
         // create production order
         int r_arr[WARRIOR_TYPES], b_arr[WARRIOR_TYPES];
         for (int i=0; i<WARRIOR_TYPES; ++i) {
@@ -222,12 +219,12 @@ int main() {
                 if (slist[i].name == BLUE_ORDER[j][0]) b_arr[j] = slist[i].strength;
             }
         }
-        
+
         Game g; // start game;
         HQ r(M, "red", r_arr);
         HQ b(M, "blue", b_arr);
-        
-        
+
+
         cout << "Case:" << case_counter << endl;
         for (g.time=0 ;!(r.end_of_prod&&b.end_of_prod); ++g.time) {
             if (!r.end_of_prod) {
@@ -243,7 +240,7 @@ int main() {
                 else cout << " blue headquarter stops making warriors" << endl;
             }
         }
-        
+
         ++case_counter;
     } while (case_counter <= n_cases);
 
